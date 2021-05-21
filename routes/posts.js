811d6272ade691a80ca3297d766ba1833c10f37e
0K0 <qdouvillez@gmail.com>
2021-05-21 06:36:25 +0200

    :sparkles: routes add

---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const postsCtrl = require('../controllers/posts');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const router = express.Router();
+
+router.post('/new', auth, multer, postsCtrl.createPost);
+router.get('/getPosts', auth, multer, postsCtrl.getAllPosts);
+router.get('/user/:id', auth, multer, postsCtrl.getPostProfile);
+router.delete('/:id', auth, multer, postsCtrl.deletePost);
+router.put('/:id/moderate', postsCtrl.moderatePost);
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../controllers/users');
+const auth = require('../middleware/auth');
+
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
+router.delete('/delete', auth, usersCtrl.deleteProfile);
+router.get('/myprofile', auth, usersCtrl.userProfile);
+router.put('/update', auth, usersCtrl.updateProfile);
+
+module.exports = router;
